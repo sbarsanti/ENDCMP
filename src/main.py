23c9b86f038e6d0b9a5881e226848cbd4b1ea611
Steve Barsanti <steve@skycatch.com>
2018-04-28 11:28:14 -0700

    Push again

--- src/main.py
+++ src/main.py
@@ -1,9 +1,20 @@
+import time
+
 from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello Don!'
+    return 'Hello Steve1!'
+
+@app.route('/foo')
+def foo():
+	x = 1+666
+	return "THE VALUE IS: %s" % x
+
+@app.route('/time')
+def timer():
+	return "The time is: %s" time.time()
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=80)
